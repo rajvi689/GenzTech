237608eaeae7c2d3ada02f28552af60bee5a0882
MIHIR VALA <mihirvala23@gmail.com>
2026-03-26 00:36:05 +0530

    removed

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { Hero } from './Hero';
-export { Services } from './Services';
-export { HowItWorks } from './HowItWorks';
-export { RequirementForm } from './RequirementForm';
-export { Testimonials } from './Testimonials';
-export { FinalCTA } from './FinalCTA';
-export { Footer } from './Footer';
